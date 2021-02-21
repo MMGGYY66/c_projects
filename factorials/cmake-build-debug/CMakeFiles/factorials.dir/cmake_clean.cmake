6a0cf5869f32204cf93f7e1719ca97f1d6d5f0c7
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2021-02-20 20:13:56 -0500

    first c

---
+++ factorials/cmake-build-debug/CMakeFiles/factorials.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factorials.dir/main.c.o"
+  "factorials"
+  "factorials.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/factorials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
